6f6bb74a136b46e1d32f97d4d64319e646c90155
sebin <sebin@geektrust.in>
2024-03-11 01:55:02 +0530

    MTE.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -31,34 +31,6 @@ export class HomeComponent implements OnInit {
   //   return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
   // }
 
-  companyLogos = [
-    {
-      imageUrlweb: '../../../../../assets/images/temp-logos/airbase-full.svg',
-      imageAlt: 'airbase',
-      name: 'airbase',
-    },
-    {
-      imageUrlweb: '../../../../../assets/images/temp-logos/bigBasket.svg',
-      imageAlt: 'bigBasket',
-    },
-    {
-      imageUrlweb: '../../../../../assets/images/temp-logos/1knetworks.svg',
-      imageAlt: '1knetworks',
-    },
-    {
-      imageUrlweb: '../../../../../assets/images/temp-logos/acceldata-full.svg',
-      imageAlt: 'HCL',
-    },
-    {
-      imageUrlweb: '../../../../../assets/images/temp-logos/Aganitha-full.svg',
-      imageAlt: 'Aganitha',
-    },
-    {
-      imageUrlweb: '../../../../../assets/images/temp-logos/bain.svg',
-      imageAlt: 'bain',
-    },
-  ];
-
   navigateToProducts(): void {
     window.open('/products-services', '_blank');
   }
