42eb2d9b3840837c1ed5682c5e8c4af7fac14d1c
sebin <sebin@geektrust.in>
2024-03-16 21:30:44 +0530

    Modifed about-us section. Added styles and new font.

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -7,6 +7,39 @@ import { NavigationStart, Router } from '@angular/router';
   styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
+  coreValues = [
+    {
+      value: 'Quality',
+      content:
+        'We are committed to offering only the highest quality marine equipment from renowned manufacturers, ensuring the reliability and durability of our products.',
+      img: 'quality.png',
+    },
+    {
+      value: 'Integrity',
+      content:
+        'We uphold the highest ethical standards in all our dealings, building trust and long-lasting relationships with clients, partners, and stakeholders.',
+      img: 'integrity.png',
+    },
+    {
+      value: 'Customer-Centric',
+      content:
+        'Our customers are at the heart of everything we do. We strive to understand their needs and deliver tailored solutions that exceed expectations.',
+      img: 'customer-centric.png',
+    },
+    {
+      value: 'Innovation',
+      content:
+        'Embracing innovation drives our growth. We continuously explore new technologies and methodologies services.',
+      img: 'innovation.png',
+    },
+    {
+      value: 'Collaboration',
+      content:
+        'We believe in the power of collaboration and actively engage with industry partners to foster mutual growth and success.',
+      img: 'collaboration.png',
+    },
+  ];
+
   constructor(private router: Router) {}
 
   ngOnInit() {
