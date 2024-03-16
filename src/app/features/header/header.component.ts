595ab3a7f7fc3a8f25d6fde3b84810602f73b9fe
sebin <sebin@geektrust.in>
2024-03-16 18:26:10 +0530

    Added products dropdown in header.

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  Products,
+  ProductsNameMap,
+} from 'src/app/core/constants/products.constants';
 
 @Component({
   selector: 'nautic-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  productNameMap = ProductsNameMap;
+  products = Products;
+  isDropdownOpen = false;
+
+  constructor(private router: Router) {}
+
+  showProducts(product: string) {
+    this.toggleDropdown();
+    this.router.navigate(['products', product]);
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+}
