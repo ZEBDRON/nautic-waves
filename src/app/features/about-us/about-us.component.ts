eb636a77a8e4424922b6f1cf007301bdc6d0aef4
sebin <sebin@geektrust.in>
2024-03-17 14:24:45 +0530

    Enhanced css styling.

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -12,31 +12,31 @@ export class AboutUsComponent implements OnInit {
       value: 'Quality',
       content:
         'We are committed to offering only the highest quality marine equipment from renowned manufacturers, ensuring the reliability and durability of our products.',
-      img: 'quality.png',
+      img: 'https://firebasestorage.googleapis.com/v0/b/nautic-waves.appspot.com/o/images%2Fship.jpg?alt=media&token=bd77c7fd-9ba4-4c78-abdf-9f4a243bcac6',
     },
     {
       value: 'Integrity',
       content:
         'We uphold the highest ethical standards in all our dealings, building trust and long-lasting relationships with clients, partners, and stakeholders.',
-      img: 'integrity.png',
+      img: 'https://firebasestorage.googleapis.com/v0/b/nautic-waves.appspot.com/o/images%2Fship2.jpg?alt=media&token=51294e59-a56a-42e5-91b9-fc5cf59e28bf',
     },
     {
       value: 'Customer-Centric',
       content:
         'Our customers are at the heart of everything we do. We strive to understand their needs and deliver tailored solutions that exceed expectations.',
-      img: 'customer-centric.png',
+      img: 'https://firebasestorage.googleapis.com/v0/b/nautic-waves.appspot.com/o/images%2Fship3.jpg?alt=media&token=b6c48f8f-8249-4903-81e2-b6514ce4b14d',
     },
     {
       value: 'Innovation',
       content:
         'Embracing innovation drives our growth. We continuously explore new technologies and methodologies services.',
-      img: 'innovation.png',
+      img: 'https://firebasestorage.googleapis.com/v0/b/nautic-waves.appspot.com/o/images%2Fship4.jpg?alt=media&token=0c28cfd6-efff-4a78-95e9-f397901fac3b',
     },
     {
       value: 'Collaboration',
       content:
         'We believe in the power of collaboration and actively engage with industry partners to foster mutual growth and success.',
-      img: 'collaboration.png',
+      img: 'https://firebasestorage.googleapis.com/v0/b/nautic-waves.appspot.com/o/images%2Fship5.jpg?alt=media&token=210b1b29-8969-40e3-a9ce-8b64ffa2326f',
     },
   ];
 
